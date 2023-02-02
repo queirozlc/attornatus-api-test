f0d3d0a90930b2b53df28734e2d6097671b90ffa
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-02-01 23:26:11 -0300

    Init: Initial commit.

---
+++ src/main/java/com/lucas/attornatustest/AttornatusTestApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.attornatustest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttornatusTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttornatusTestApplication.class, args);
+	}
+
+}
