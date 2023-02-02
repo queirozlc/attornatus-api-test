605ae11b01369a51291629c9d154dbae7389e039
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-02-02 01:02:11 -0300

    feat: Address Section
    Initial code from address section

---
+++ src/main/java/com/lucas/attornatustest/entity/Address.java
@@ -0,0 +1,27 @@
+package com.lucas.attornatustest.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "address")
+public class Address {
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+	private String publicSpace;
+	private String streetAddress;
+	private Integer number;
+	private String city;
+	@ManyToOne(targetEntity = Person.class, fetch = FetchType.LAZY)
+	private Person person;
+}
