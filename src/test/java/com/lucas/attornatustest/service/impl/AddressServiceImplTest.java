37da794069aed72b2cc5c1ef173c42e0614fb6a1
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-02-06 23:15:57 -0300

    test: Unit tests refactoring

--- src/test/java/com/lucas/attornatustest/service/impl/AddressServiceImplTest.java
+++ src/test/java/com/lucas/attornatustest/service/impl/AddressServiceImplTest.java
@@ -3,61 +3,131 @@ package com.lucas.attornatustest.service.impl;
 import com.lucas.attornatustest.entity.Address;
 import com.lucas.attornatustest.entity.Person;
 import com.lucas.attornatustest.exception.bad_request.BadRequestException;
+import com.lucas.attornatustest.mapper.AddressMapper;
 import com.lucas.attornatustest.repository.AddressRepository;
 import com.lucas.attornatustest.repository.PersonRepository;
 import com.lucas.attornatustest.request.AddressRequestBody;
-import static com.lucas.attornatustest.util.TestUtils.*;
-
-import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(SpringExtension.class)
 class AddressServiceImplTest {
+	public static final UUID ID = UUID.fromString("7c5c3dac-d966-4030-8aa5-809529298c1e");
+	public static final LocalDate BIRTH_DATE = LocalDate.parse("2007-12-03");
+	public static final String NAME = "person";
+	public static final String PERSON_DONT_EXIST = "Não existe nenhuma pessoa com id informado.";
+	public static final String CITY = "New York";
+	public static final String STREET_ADDRESS = "123 Main Street";
+	public static final String ZIP_CODE = "12345";
+	public static final int NUMBER = 10;
+	public static Person person;
+	public static Optional<Person> personOptional;
+	public static AddressRequestBody addressRequestBody;
+	public static Address address;
+	public static List<Address> addresses;
 
+	@InjectMocks
+	private AddressServiceImpl addressService;
 	@Mock
 	private AddressRepository repository;
 	@Mock
 	private PersonRepository personRepository;
+	@Mock
+	private AddressMapper mapper;
 
-	@InjectMocks
-	private AddressServiceImpl service;
-
-	@Test
-	@DisplayName("should create an address with a person as owner.")
-	void createAddressWithPersonAsOwner() {
-		AddressRequestBody addressRequestBody = createAddressDto();
-		Person person = createPersonWithoutMainAddressToSave();
-		person.setId(addressRequestBody.getPersonId());
-
-		Mockito.when(personRepository.findById(addressRequestBody.getPersonId()))
-						.thenReturn(Optional.of(person));
-
-		service.createAddress(addressRequestBody);
-
-		Mockito.verify(repository, Mockito.times(1))
-				.save(ArgumentMatchers.any(Address.class));
+	@BeforeEach
+	void setUp() {
+		initClasses();
 	}
 
 	@Test
-	@DisplayName("Should throw a Bad Request exception when try to find an unknown person id.")
-	void createAddressThrowBadRequestExceptionWhenTryToFindAnUnknownPersonId() {
-		AddressRequestBody addressDto = createAddressDto();
-		Person person = createPersonWithMainAddressToSave();
-		person.setId(UUID.randomUUID());
+	@DisplayName("Create address when successful")
+	void createAddressWhenSuccessful() {
+		when(mapper.toAddress(any(AddressRequestBody.class))).thenReturn(address);
+		when(repository.save(any(Address.class))).thenReturn(address);
+		when(personRepository.findById(any(UUID.class))).thenReturn(personOptional);
 
-		Mockito.when(personRepository.findById(addressDto.getPersonId()))
-				.thenThrow(BadRequestException.class);
+		Address response = addressService.createAddress(addressRequestBody);
 
-		assertThatException().isThrownBy(() -> service.createAddress(addressDto));
+		assertNotNull(response);
+		assertEquals(Address.class, response.getClass());
+		assertEquals(UUID.class, response.getId().getClass());
+		assertEquals(Person.class, address.getPerson().getClass());
+	}
+
+	@Test
+	@DisplayName("Create address throws bad request exception when person doesn't found")
+	void createAddressThrowsBadRequestExceptionWhenPersonDoesNotExist() {
+		when(mapper.toAddress(any(AddressRequestBody.class))).thenReturn(address);
+		when(repository.save(any(Address.class))).thenReturn(address);
+		when(personRepository.findById(any(UUID.class))).thenReturn(Optional.empty());
+
+		Exception exception = assertThrows(BadRequestException.class,
+				() -> addressService.createAddress(addressRequestBody));
+
+		assertEquals(PERSON_DONT_EXIST, exception.getMessage());
+	}
+
+	@Test
+	@DisplayName("List all addresses by person owner when successful")
+	void listAllAddressesByPersonOwnerWhenSuccessful() {
+		when(personRepository.findById(any(UUID.class))).thenReturn(personOptional);
+		when(repository.findAllByPerson(any(Person.class))).thenReturn(addresses);
+
+		List<Address> response = addressService.listAllAddressByPerson(ID);
+
+		assertNotNull(response);
+		assertEquals(1, response.size());
+		assertEquals(Address.class, response.get(0).getClass());
+		assertEquals(UUID.class, response.get(0).getId().getClass());
+		assertEquals(Person.class, response.get(0).getPerson().getClass());
+	}
+
+	@Test
+	@DisplayName("List all addresses by person owner throws bad request exception when person doesn't found")
+	void listAllAddressesByPersonOwnerThrowsBadRequestExceptionWhenPersonDoesNotExist() {
+		when(personRepository.findById(any(UUID.class))).thenReturn(Optional.empty());
+
+		Exception exception = assertThrows(BadRequestException.class,
+				() -> addressService.listAllAddressByPerson(ID));
+
+		assertEquals(PERSON_DONT_EXIST, exception.getMessage());
+	}
+
+	private void initClasses() {
+		person = new Person(ID, NAME, BIRTH_DATE, new Address());
+		personOptional = Optional.of(person);
+		addressRequestBody = AddressRequestBody
+				.builder()
+				.personId(ID)
+				.city(CITY)
+				.number(NUMBER)
+				.streetAddress(STREET_ADDRESS)
+				.zipCode(ZIP_CODE)
+				.build();
+		address = Address
+				.builder()
+				.id(ID)
+				.person(person)
+				.number(NUMBER)
+				.zipCode(ZIP_CODE)
+				.city(CITY)
+				.streetAddress(STREET_ADDRESS)
+				.build();
+		addresses = List.of(address);
 	}
 }
--- src/test/java/com/lucas/attornatustest/service/impl/PersonServiceImplTest.java
+++ src/test/java/com/lucas/attornatustest/service/impl/PersonServiceImplTest.java
@@ -2,113 +2,243 @@ package com.lucas.attornatustest.service.impl;
 
 import com.lucas.attornatustest.entity.Address;
 import com.lucas.attornatustest.entity.Person;
+import com.lucas.attornatustest.exception.bad_request.BadRequestException;
+import com.lucas.attornatustest.exception.date_parse_exception.DateParseException;
+import com.lucas.attornatustest.mapper.PersonMapper;
 import com.lucas.attornatustest.repository.AddressRepository;
 import com.lucas.attornatustest.repository.PersonRepository;
 import com.lucas.attornatustest.request.PersonRequestBody;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import static com.lucas.attornatustest.util.TestUtils.*;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 class PersonServiceImplTest {
-
+	public static final UUID ID = UUID.fromString("7c5c3dac-d966-4030-8aa5-809529298c1e");
+	public static final LocalDate BIRTH_DATE = LocalDate.parse("2007-12-03");
+	public static final String NAME = "person";
+	public static final String REQUEST_BIRTH_DATE = "04/12/2003";
+	public static final String PERSON_DONT_EXIST = "Não existe nenhuma pessoa com esse id.";
+	public static Person person;
+	public static Optional<Person> personOptional;
+	public static PersonRequestBody personRequestBody;
+	public static Address address;
+	public static List<Person> personList;
 
 	@InjectMocks
 	private PersonServiceImpl service;
 	@Mock
-	private PersonRepository personRepository;
+	private PersonRepository repository;
 	@Mock
 	private AddressRepository addressRepository;
+	@Mock
+	private PersonMapper mapper;
 
-
-
-	@Test
-	void createPersonWhenSuccessful() {
-		Person personToBeSaved = createPersonWithoutMainAddressToSave();
-		PersonRequestBody personRequestBody = toDto(personToBeSaved);
-
-		Mockito.when(personRepository.save(personToBeSaved)).thenReturn(personToBeSaved);
-
-		Person personSaved = service.createPerson(personRequestBody);
-
-		assertThat(personSaved).isNotNull();
-		assertThat(personSaved.getName()).isEqualTo(personToBeSaved.getName());
+	@BeforeEach
+	void setUp() {
+		initClasses();
 	}
 
 	@Test
+	@DisplayName("Create person without main address when successful")
+	void createPersonWithoutMainAddressWhenSuccessful() {
+		when(mapper.toPerson(any(PersonRequestBody.class))).thenReturn(person);
+		when(repository.save(any(Person.class))).thenReturn(person);
+
+		Person response = service.createPerson(personRequestBody);
+
+		assertNotNull(response);
+		assertEquals(Person.class, response.getClass());
+		assertEquals(UUID.class, response.getId().getClass());
+		assertEquals(NAME, response.getName());
+		verify(repository, times(1)).save(person);
+	}
+
+	@Test
+	@DisplayName("Create person with main address when successful")
+	void createPersonWithMainAddressWhenSuccessful() {
+		when(mapper.toPerson(any(PersonRequestBody.class))).thenReturn(person);
+		when(repository.save(any(Person.class))).thenReturn(person);
+		when(addressRepository.save(any(Address.class))).thenReturn(address);
+		person.setMainAddress(address);
+
+		Person response = service.createPerson(personRequestBody);
+
+		assertNotNull(response);
+		assertEquals(Person.class, response.getClass());
+		assertEquals(UUID.class, response.getId().getClass());
+		assertEquals(NAME, response.getName());
+		verify(repository, times(1)).save(person);
+	}
+
+	@Test
+	@DisplayName("Create person throws date parse exception when birthDate is invalid")
+	void createPersonThrowsDateParseExceptionWhenBirthDateIsInvalid() {
+		when(mapper.toPerson(any(PersonRequestBody.class))).thenThrow(DateParseException.class);
+
+		try {
+			personRequestBody.setBirthDate("1/13/2010");
+			mapper.toPerson(personRequestBody);
+		}
+		catch (Exception e) {
+			assertEquals(DateParseException.class, e.getClass());
+		}
+	}
+
+	@Test
+	@DisplayName("Create person throws bad request exception when birthDate is greater than current date")
+	void createPersonThrowsBadRequestExceptionWhenBirthDateIsGreaterThanCurrentDate() {
+		when(mapper.toPerson(any(PersonRequestBody.class))).thenThrow(DateParseException.class);
+
+		try {
+			personRequestBody.setBirthDate(LocalDate.now().plusDays(1).toString());
+			mapper.toPerson(personRequestBody);
+		}
+		catch (Exception e) {
+			assertEquals(DateParseException.class, e.getClass());
+		}
+	}
+
+	@Test
+	@DisplayName("Update person when successful")
 	void updatePersonWhenSuccessful() {
-		UUID id = UUID.fromString("0f0da5e1-daf8-40ba-b04b-000ea58a4f62");
-		Person personToBeSaved = createPersonWithoutMainAddressToSave();
-		PersonRequestBody personRequestBody = createDto();
-		personToBeSaved.setName(personRequestBody.getName());
-		personToBeSaved.setBirthDate(LocalDate.parse(personRequestBody.getBirthDate(), DateTimeFormatter.ofPattern(
-				"dd/MM/yyyy")));
+		when(repository.findById(any(UUID.class))).thenReturn(personOptional);
+		when(mapper.toPerson(any(PersonRequestBody.class))).thenReturn(person);
+		when(repository.save(any(Person.class))).thenReturn(person);
 
-		Mockito.when(personRepository.findById(id)).thenReturn(Optional.of(personToBeSaved));
+		Person response = service.updatePerson(personRequestBody, ID);
 
-		service.updatePerson(personRequestBody, id);
-
-		Mockito.verify(personRepository, Mockito.times(1))
-				.save(personToBeSaved);
-	}
-
-
-	@Test
-	void findOneByIdWhenSuccessful() {
-		UUID id = UUID.fromString("0f0da5e1-daf8-40ba-b04b-000ea58a4f62");
-		Person personToBeSaved = createPersonWithoutMainAddressToSave();
-		personToBeSaved.setId(id);
-
-		Mockito.when(personRepository.findById(id)).thenReturn(Optional.of(personToBeSaved));
-
-		Person personFound = service.findOneById(id);
-
-		assertThat(personFound).isNotNull();
-		assertThat(personFound.getId())
-				.isNotNull()
-				.isEqualTo(personToBeSaved.getId());
+		assertNotNull(response);
+		assertEquals(Person.class, response.getClass());
+		assertEquals(UUID.class, response.getId().getClass());
+		assertEquals(NAME, response.getName());
+		verify(repository, times(1)).save(person);
 	}
 
 	@Test
-	void listAllAddressFromPersonWhenSuccessful() {
-		Person personToBeSaved = createPersonWithoutMainAddressToSave();
-		UUID id = UUID.fromString("0138f1cf-af33-4f16-b90b-a2415ab39e39");
-		personToBeSaved.setId(id);
-		Address addressToSave = createAddressToSave();
+	@DisplayName("Update person with main address when successful")
+	void updatePersonWithMainAddressWhenSuccessful() {
+		when(repository.findById(any(UUID.class))).thenReturn(personOptional);
+		when(mapper.toPerson(any(PersonRequestBody.class))).thenReturn(person);
+		when(repository.save(any(Person.class))).thenReturn(person);
+		when(addressRepository.save(any(Address.class))).thenReturn(address);
+		person.setMainAddress(address);
 
-		Mockito.when(personRepository.findById(id)).thenReturn(Optional.of(personToBeSaved));
-		Mockito.when(addressRepository.findAllByPerson(personToBeSaved))
-				.thenReturn(List.of(addressToSave));
+		Person response = service.updatePerson(personRequestBody, ID);
 
-		List<Address> addresses = service.listAllAddressByPerson(id);
-
-		assertThat(addresses).isNotEmpty();
+		assertNotNull(response);
+		assertEquals(Person.class, response.getClass());
+		assertEquals(UUID.class, response.getId().getClass());
+		assertEquals(NAME, response.getName());
+		verify(repository, times(1)).save(person);
 	}
 
+	@Test
+	@DisplayName("Update person throws bad request exception when no person found.")
+	void updatePersonThrowsBadRequestExceptionWhenNoPersonFound() {
+		when(repository.findById(any(UUID.class))).thenReturn(Optional.empty());
+
+		try {
+			service.updatePerson(personRequestBody, ID);
+		}
+		catch (Exception e) {
+			assertEquals(BadRequestException.class, e.getClass());
+			assertEquals(PERSON_DONT_EXIST, e.getMessage());
+		}
+	}
 
 	@Test
+	@DisplayName("Update person throws date parse exception when birthDate is invalid")
+	void updatePersonThrowsDateParseExceptionWhenBirthDateIsInvalid() {
+		when(repository.findById(any(UUID.class))).thenReturn(personOptional);
+		when(mapper.toPerson(any(PersonRequestBody.class))).thenThrow(DateParseException.class);
+
+		try {
+			personRequestBody.setBirthDate("1/13/2010");
+			mapper.toPerson(personRequestBody);
+		}
+		catch (Exception e) {
+			assertEquals(DateParseException.class, e.getClass());
+		}
+	}
+
+	@Test
+	@DisplayName("Update person throws bad request exception when birthDate is greater than current date")
+	void updatePersonThrowsBadRequestExceptionWhenBirthDateIsGreaterThanCurrentDate() {
+		when(repository.findById(any(UUID.class))).thenReturn(personOptional);
+		when(mapper.toPerson(any(PersonRequestBody.class))).thenThrow(DateParseException.class);
+
+		try {
+			personRequestBody.setBirthDate(LocalDate.now().plusDays(1).toString());
+			mapper.toPerson(personRequestBody);
+		}
+		catch (Exception e) {
+			assertEquals(DateParseException.class, e.getClass());
+		}
+	}
+
+	@Test
+	@DisplayName("Find one person by id when successful")
+	void findOnePersonByIdWhenSuccessful() {
+		when(repository.findById(any(UUID.class))).thenReturn(personOptional);
+		Person response = service.findOneById(ID);
+
+		assertEquals(Person.class, response.getClass());
+		assertEquals(NAME, response.getName());
+		assertEquals(BIRTH_DATE, response.getBirthDate());
+	}
+
+	@Test
+	@DisplayName("Find one throws bad request exception when no person found.")
+	void findOneThrowsBadRequestExceptionWhenNoPersonFound() {
+		when(repository.findById(any(UUID.class))).thenReturn(Optional.empty());
+
+		try {
+			service.findOneById(ID);
+		}
+		catch (Exception e) {
+			assertEquals(BadRequestException.class, e.getClass());
+			assertEquals(PERSON_DONT_EXIST, e.getMessage());
+		}
+	}
+
+	@Test
+	@DisplayName("Find all Persons when successful")
 	void findAllPersonsWhenSuccessful() {
-		List<Person> personsList = List.of(createPersonWithoutMainAddressToSave());
-		Mockito.when(personRepository.findAll()).thenReturn(personsList);
+		when(repository.findAll()).thenReturn(personList);
+		List<Person> response = service.findAll();
 
-		List<Person> personList = service.findAll();
-		assertThat(personList)
-				.isNotNull()
-				.isNotEmpty()
-				.hasSize(1);
-		assertThat(personList.get(0).getName()).isEqualTo("Caleb César Rocha");
+		assertEquals(Person.class, response.get(0).getClass());
+		assertEquals(NAME, response.get(0).getName());
+		assertEquals(1, response.size());
 	}
 
+	private void initClasses() {
+		person = new Person(ID, NAME, BIRTH_DATE, new Address());
+		personOptional = Optional.of(person);
+		personRequestBody = new PersonRequestBody(NAME, REQUEST_BIRTH_DATE, null);
+		personList = List.of(person);
+		address = Address
+				.builder()
+				.person(person)
+				.number(10)
+				.zipCode("12345")
+				.city("New York")
+				.streetAddress("123 Main Street")
+				.build();
+	}
 }
